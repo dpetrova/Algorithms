10e419caab3813ae4e55456c141e19fa05b08909
dpetrova <daniella_petrova@abv.bg>
2020-01-23 19:39:24 +0200

    variations algorithms

---
+++ Combinatorial/04_variations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_variations
+{
+    class Program
+    {
+        static int[] elements;
+        static int[] variations;
+        static bool[] used;
+
+        static void Main()
+        {
+            int n = 3;
+            int k = 2;
+            elements = new int[] { 1, 2, 3 };            
+            variations = new int[k];
+            used = new bool[n];
+            Variate(0);
+        }
+
+        static void Variate(int index)
+        {
+            if(index == variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        variations[index] = elements[i];
+                        used[i] = true;
+                        Variate(index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Combinatorial/05_variations_with_repetitions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace _05_variations_with_repetitions
+{
+    class Program
+    {
+        static int[] elements;
+        static int[] variations;
+
+        static void Main()
+        {
+            int n = 3;
+            int k = 2;
+            elements = new int[] { 1, 2, 3 };
+            variations = new int[k];
+            Variate(0);
+        }
+
+        static void Variate(int index)
+        {
+            if(index == variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    variations[index] = elements[i];
+                    Variate(index + 1);
+                }
+            }
+        }
+    }
+}
