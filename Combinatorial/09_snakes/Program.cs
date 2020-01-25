0a517abe0ef8973d350c15b01edf0759b8a60dfe
dpetrova <daniella_petrova@abv.bg>
2020-01-25 19:20:53 +0200

    some exercises

---
+++ Combinatorial/09_snakes/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09_snakes
+{
+    class Program
+    {
+        static int n;
+        static Stack<Cell> snake;
+        static List<char> directions;
+        static HashSet<string> used; //unordered collection of unique elements
+
+        static void Main()
+        {
+            n = 4;
+            snake = new Stack<Cell>();
+            directions = new List<char>();
+            used = new HashSet<string>();
+            GenerateSnake(new Cell(0, 0), 'S');
+        }
+
+        static void GenerateSnake(Cell cell, char direction)
+        {
+            if (snake.Count == n)
+            {
+                string currentSnake = new string(directions.ToArray());
+                if (!used.Contains(currentSnake))
+                {
+                    Console.WriteLine(currentSnake);
+                    used.Add(currentSnake);
+                    CheckIsomorphic(directions);
+                }
+
+                return;
+            }
+
+            if (snake.Contains(cell))
+            {
+                return;
+            }
+            else
+            {
+                snake.Push(cell);
+                directions.Add(direction);
+
+                // generate in right direction
+                Cell right = new Cell(cell.Row, cell.Col + 1);
+                GenerateSnake(right, 'R');
+
+                // generate in down direction
+                Cell down = new Cell(cell.Row + 1, cell.Col);
+                GenerateSnake(down, 'D');
+
+                // generate in left direction
+                Cell left = new Cell(cell.Row, cell.Col - 1);
+                GenerateSnake(left, 'L');
+
+                // generate in up direction
+                Cell up = new Cell(cell.Row - 1, cell.Col);
+                GenerateSnake(up, 'U');
+
+                snake.Pop();
+                directions.RemoveAt(directions.Count - 1);
+            }
+        }
+
+        static void CheckIsomorphic(List<char> directions)
+        {
+            //flip snake
+            Flip(directions);
+            if(!used.Contains(new string(directions.ToArray())))
+            {
+                used.Add(new string(directions.ToArray()));
+            }
+
+            //unflip snake
+            Flip(directions);
+
+            //rotatie until reach initial state (5th returns to initial state)
+            for (int i = 0; i <= 5; i++)
+            {
+                RotateClockwise(directions);
+                if (!used.Contains(new string(directions.ToArray())))
+                {
+                    used.Add(new string(directions.ToArray()));
+                }
+            }
+        }
+
+        //flip mirror
+        //  xx  ->  xx
+        //  x        x 
+        static void Flip(List<char> directions)
+        {
+            for (int i = 0; i < directions.Count; i++)
+            {
+                switch (directions[i])
+                {
+                    case 'L': directions[i] = 'R'; break;
+                    case 'R': directions[i] = 'L'; break;
+                }
+            }
+        }
+
+        //rotate clickwise
+        //  xx  ->  xx  ->   x  -> x    -> xx
+        //  x        x      xx     xx      x
+        public static void RotateClockwise(List<char> directions)
+        {
+            for (int i = 0; i < directions.Count; i++)
+            {
+                switch (directions[i])
+                {
+                    case 'R': directions[i] = 'D'; break;
+                    case 'L': directions[i] = 'U'; break;
+                    case 'U': directions[i] = 'R'; break;
+                    case 'D': directions[i] = 'L'; break;
+                }
+            }
+        }
+    }
+
+
+    class Cell
+    {
+        int row;
+        int col;
+
+        public Cell(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+    }
+
+}
+
+
+
+
+
+
+
---
+++ Combinatorial/10_cubes/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_cubes
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] colors = new int[] { 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+            Cube cube = new Cube();
+            foreach (var item in cube.Vertices)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            foreach (var item in cube.Edges)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+
+    
+    class Cube
+    {        
+        int[] elements = new int[] { 0, 1 };
+        int[] currVertex = new int[3];  //x,y,z
+        Vertex[] currEdge = new Vertex[2]; //start, end of edge
+        List<Vertex> vertices = new List<Vertex>();
+        List<Edge> edges = new List<Edge>();
+                
+        public Cube()
+        {
+            //generate vertices
+            GenerateVertices(0, 0);
+
+            //generate edges
+            GenerateEdges(0, 0);
+        }
+
+        public List<Vertex> Vertices {
+            get { return this.vertices; }
+            set
+            {                
+                this.vertices = value;
+            }
+        }
+
+        public List<Edge> Edges
+        {
+            get { return this.edges; }
+            set
+            {
+                this.edges = value;
+            }
+        }
+
+        // each cube consist of vertices that are unique combinations of xyz (represented by 0 or 1)
+        // to generate all verices -> variations with repetitions (n elements on k slots)
+        // n = 2 (0 or 1), k = 3 (x, y, or z) => 2^3 = 8
+        void GenerateVertices(int index, int start)
+        {
+            if (index == this.currVertex.Length)
+            {
+                this.Vertices.Add(new Vertex(this.currVertex[0], this.currVertex[1], this.currVertex[2]));
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < this.elements.Length; i++)
+                {
+                    this.currVertex[index] = this.elements[i];
+                    GenerateVertices(index + 1, start);
+                }
+            }
+        }
+
+        // an edge consist from two vertices, in which one axis is changed and the others two remain same
+        // edges are combinations without repetitions of vertices (8 vertices on 2 slots)
+        // with some constraint that no all possible combinations can form en edge (e.g diagonals are not edges)
+        void GenerateEdges(int index, int start)
+        {
+            if (index == this.currEdge.Length)
+            {
+                if(Edge.IsPossibleEdge(this.currEdge[0], this.currEdge[1]))
+                {
+                    this.Edges.Add(new Edge(this.currEdge[0], this.currEdge[1]));
+                }
+                
+                return;
+            }
+            else
+            {
+                for (int i = start; i < this.vertices.Count; i++)
+                {
+                    this.currEdge[index] = this.vertices[i];
+                    GenerateEdges(index + 1, i + 1);                    
+                }
+            }
+        }        
+    }
+
+    class Edge
+    {
+        Vertex start;
+        Vertex end;
+        int color;
+
+        public Edge(Vertex start, Vertex end) : this(start, end, 0)
+        {            
+        }
+
+        public Edge(Vertex start, Vertex end, int color)
+        {
+            this.Start = start;
+            this.End = end;
+            this.Color = color;
+        }
+
+        public Vertex Start { get; set; }
+        public Vertex End { get; set; }
+        public int Color { get; set; }
+
+        public static bool IsPossibleEdge(Vertex start, Vertex end)
+        {
+            //the edge is possible if one direction between the two vertices is changed, and the others remain the same
+            return (start.X != end.X && start.Y == end.Y && start.Z == end.Z) || //change of X axis
+                    (start.X == end.X && start.Y != end.Y && start.Z == end.Z) || //change of Y axis
+                    (start.X == end.X && start.Y == end.Y && start.Z != end.Z); //change of Z axis
+        }
+
+        public override bool Equals(object obj)
+        {
+            Edge other = obj as Edge;           
+            return (this.Start.Equals(other.Start) && this.End.Equals(other.End)) ||
+                    (this.Start.Equals(other.End) && this.End.Equals(other.Start));
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Edge: {0}, {1}", this.Start, this.End);
+        }
+    }
+
+    class Vertex
+    {
+        int x;
+        int y;
+        int z;
+
+        //each value can be either 0 or 1
+        public Vertex(int x, int y, int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Z { get; set; }         
+
+        public override bool Equals(object obj)
+        {
+            Vertex other = obj as Vertex;
+            return this.X == other.X && this.Y == other.Y && this.Z == other.Z;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Vertex: {0}{1}{2}", this.X, this.Y, this.Z);
+        }
+    }    
+}
