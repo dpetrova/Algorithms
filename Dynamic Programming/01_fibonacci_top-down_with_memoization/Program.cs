6b01e8c2036fb7e79249b707dd0229463e1eb4d1
dpetrova <daniella_petrova@abv.bg>
2020-02-02 20:15:28 +0200

    DP -> fibonacci top-down recursive with memoization

---
+++ Dynamic Programming/01_fibonacci_top-down_with_memoization/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+//Fi
+namespace _01_fibonacci_top_down_with_memoization
+{
+    class Program
+    {
+        static int[] memoization;
+        static int n;
+
+        static void Main()
+        {
+            n = 5;
+            memoization = new int[n + 1];
+            Console.WriteLine(Fibonacci(n)); 
+        }
+
+        static int Fibonacci(int n)
+        {
+            //already calculate it
+            if(memoization[n] != 0)
+            {
+                return memoization[n];
+            }
+
+            //first fibonacci element is 0
+            //second and third are 1
+            if(n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            var result = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+            //cash result
+            memoization[n] = result;
+
+            return result;
+        }
+    }
+}
