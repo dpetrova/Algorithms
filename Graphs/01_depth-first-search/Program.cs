710aef88b430117e6c7264fb14ec4a1e621f90fb
dpetrova <daniella_petrova@abv.bg>
2020-02-24 20:09:25 +0200

    dfs and bfs traversal graphs algorithms

---
+++ Graphs/01_depth-first-search/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Depth first search (DFS) for graph traversal
+ 
+namespace _01_depth_first_search
+{
+    class Program
+    {
+        static List<int>[] graph;
+        static bool[] visited;
+
+        static void Main()
+        {
+            //every list represents a node
+            graph = new List<int>[]
+            {
+                new List<int>{1, 3, 5}, //node 0
+                new List<int>{2},       //node 1
+                new List<int>{},        //node 2
+                new List<int>{4},       //node 3
+                new List<int>{},        //node 4
+                new List<int>{}         //node 5
+                
+            };
+
+            visited = new bool[graph.Length];
+
+            DFS(0);
+            //for (int i = 0; i < graph.Length; i++)
+            //{
+            //    //perform DFS for every node
+            //    DFS(i);
+            //}
+        }
+
+        static void DFS(int currentNode)
+        {
+            //check if node is already visited
+            if(!visited[currentNode])
+            {
+                visited[currentNode] = true;
+                //first parent node from which started, last printed deepest child
+                //Console.Write($"{currentNode} ");
+
+                //recursively traversal all children of the node
+                foreach (var child in graph[currentNode])
+                {
+                    DFS(child);
+                }
+
+                //first print deepest node, last print those node from which started
+                Console.Write($"{currentNode} ");
+            }
+        }
+    }
+}
---
+++ Graphs/02_breadth-first-search/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Breadth first search (BFS) for graph traversal
+
+namespace _02_breadth_first_search
+{
+    class Program
+    {
+        static List<int>[] graph;
+        static bool[] visited;
+
+        static void Main()
+        {
+            //every list represents a node
+            graph = new List<int>[]
+            {
+                new List<int>{1, 3, 5}, //node 0
+                new List<int>{2},       //node 1
+                new List<int>{},        //node 2
+                new List<int>{4},       //node 3
+                new List<int>{},        //node 4
+                new List<int>{}         //node 5
+                
+            };
+
+            visited = new bool[graph.Length];
+
+            BFS(0);
+            //for (int i = 0; i < graph.Length; i++)
+            //{
+            //    //perform DFS for every node
+            //    BFS(i);
+            //}
+        }
+
+        static void BFS(int node)
+        {
+            var queue = new Queue<int>();
+
+            //add current node in the queue
+            queue.Enqueue(node);
+            visited[node] = true;
+
+            //while there are nodes in the queue
+            while (queue.Count > 0)
+            {
+                //remove last node from the queue
+                var currNode = queue.Dequeue();
+
+                //print curr node
+                Console.Write($"{currNode} ");
+
+                //traverse all children of the node
+                foreach (var child in graph[currNode])
+                {
+                    if(!visited[child])
+                    {
+                        // add each child in the queue
+                        queue.Enqueue(child);
+                        visited[child] = true;
+                    }
+                    
+                }
+            }
+           
+        }
+    }
+}
